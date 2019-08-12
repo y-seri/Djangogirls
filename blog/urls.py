765e0da62c1e750312c687606389025fe15e7917
yuki serizawa <yukiserizawa0@gmail.com>
2019-08-13 01:40:21 +0900

    Revert "change urls.py"
    This reverts commit 099ee032bc6fa283859059a86eafb5be8acf23a3.

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,6 @@
-from django.urls import path, include
-from django.contrib import admin
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('blog.urls')),
+    path('', views.post_list, name='post_list'),
 ]
