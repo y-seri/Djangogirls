099ee032bc6fa283859059a86eafb5be8acf23a3
yuki serizawa <yukiserizawa0@gmail.com>
2019-08-13 01:37:18 +0900

    change urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
-from . import views
+from django.urls import path, include
+from django.contrib import admin
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
